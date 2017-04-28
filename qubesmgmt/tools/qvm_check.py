6e844220620387f3f7d7166a9bf9c40a0e791bed
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2017-04-28 02:14:55 +0200

    tools: minor fix for qvm-check

--- qubesmgmt/tools/qvm_check.py
+++ qubesmgmt/tools/qvm_check.py
@@ -40,8 +40,7 @@ parser.add_argument("--template", action="store_true", dest="template",
 
 def print_msg(domains, what_single, what_plural):
     '''Print message in appropriate form about given domain(s)'''
-    # pylint: disable=len-as-condition
-    if len(domains) == 0:
+    if not domains:
         print("None of given VM {!s}".format(what_single))
     elif len(domains) == 1:
         print("VM {!s} {!s}".format(domains[0], what_single))
