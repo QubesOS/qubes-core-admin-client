0caf6f735d65dba04ed8db048f520e6f64fd07b6
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2018-06-27 01:23:56 +0200

    Fix reporting events even if its source is not available anymore
    This is especially the case for domain-shutdown event for DispVM - when
    handled, DispVM can be already removed. Give the handled VM object even
    if actual VM is already removed. For this, avoid VM existence check by
    using domains.get_blind() method.
    
    And actually implement domains.get_blind() method - it was present only
    in generic collection, but not VMCollection.

--- qubesadmin/app.py
+++ qubesadmin/app.py
@@ -85,6 +85,13 @@ class VMCollection(object):
     def __getitem__(self, item):
         if not self.app.blind_mode and item not in self:
             raise KeyError(item)
+        return self.get_blind(item)
+
+    def get_blind(self, item):
+        '''
+        Get a vm without downloading the list
+        and checking if exists
+        '''
         if item not in self._vm_objects:
             cls = qubesadmin.vm.QubesVM
             # provide class name to constructor, if already cached (which can be
