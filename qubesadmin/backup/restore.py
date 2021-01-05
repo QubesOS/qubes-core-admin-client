e3601604561907903ac242e89ef865c5ae4c99ed
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2021-01-05 03:31:09 +0100

    backup: add function parameter for override temp directory
    This will be useful for tests.
    
    QubesOS/qubes-issues#
    
    (cherry picked from commit d906fbc4bb4ccfc6d1ac32fe600bcae34d548737)

--- qubesadmin/backup/restore.py
+++ qubesadmin/backup/restore.py
@@ -20,6 +20,7 @@
 
 '''Backup restore module'''
 
+import contextlib
 import errno
 import fcntl
 import functools
@@ -910,7 +911,8 @@ class BackupRestore(object):
                 self.username = os.path.basename(subdir)
 
     def __init__(self, app, backup_location, backup_vm, passphrase,
-                 force_compression_filter=None):
+                 force_compression_filter=None,
+                 tmpdir="/var/tmp"):
         super(BackupRestore, self).__init__()
 
         #: qubes.Qubes instance
@@ -936,7 +938,7 @@ class BackupRestore(object):
 
         #: temporary directory used to extract the data before moving to the
         # final location
-        self.tmpdir = tempfile.mkdtemp(prefix="restore", dir="/var/tmp")
+        self.tmpdir = tempfile.mkdtemp(prefix="restore", dir=tmpdir)
 
         #: list of processes (Popen objects) to kill on cancel
         self.processes_to_kill_on_cancel = []
