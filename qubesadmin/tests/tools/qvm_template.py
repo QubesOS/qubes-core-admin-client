cf423845519756fcae7e0cd529a1dc8b1f8c0cda
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2024-12-12 17:17:39 +0100

    Add copyright header to qvm_template tests

--- qubesadmin/tests/tools/qvm_template.py
+++ qubesadmin/tests/tools/qvm_template.py
@@ -1,3 +1,23 @@
+# -*- encoding: utf8 -*-
+#
+# The Qubes OS Project, http://www.qubes-os.org
+#
+# Copyright (C) 2020 WillyPillow <wp@nerde.pw>
+# Copyright (C) 2024 Marek Marczykowski-Górecki
+#                               <marmarek@invisiblethingslab.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation; either version 2.1 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License along
+# with this program; if not, see <http://www.gnu.org/licenses/>.
 import re
 from unittest import mock, skipUnless
 import argparse
