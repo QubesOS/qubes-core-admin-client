QubesOS/qubes-core-admin-client

commit c95a89cb9aa9179a8d4fd57480a3a3c304c6de8f

devices: invalidate device cache on device-removed event
Avoid returning old device when it got removed already. This also
(hopefully) fixes event handlers getting VirtualDevice instead of real
one:

    Failed to handle event: sys-usb, device-added:usb, {'device': sys-usb+4-1:0bda:8179:00E04C0001:uffffff}
    Traceback (most recent call last):
      File "/usr/lib/python3.13/site-packages/qubesadmin/events/__init__.py", line 278, in handle
        kwargs['device'] = plugged
        ^^^^^^^^^^^^^^^^^^^^^
      File "/usr/lib/python3.13/site-packages/qui/devices/device_widget.py", line 262, in device_added
        dev = backend.Device(device, self)
      File "/usr/lib/python3.13/site-packages/qui/devices/backend.py", line 156, in __init__
        for interface in dev.interfaces:
                         ^^^^^^^^^^^^^^
    AttributeError: 'VirtualDevice' object has no attribute 'interfaces'

qubesadmin/events/__init__.py
@@ -257,6 +257,12 @@ class EventsDispatcher(object):
         ):
             devclass = event.split(":")[1]
             subject.devices[devclass]._attachment_cache = None
+        elif event.split(":")[0] in ("device-removed",):
+            devclass = event.split(":")[1]
+            try:
+                subject.devices[devclass]._dev_cache[kwargs["port"]]
+            except KeyError:
+                pass
 
         # deserialize known attributes
         if event.startswith('device-'):

commit 69b86a95a4bdcb9ae464fc862b384117a0556384

Fix invalidating cache on device-removed event
With broken cache invalidation, plugging new device in the same port as
an old one returned stale info from cache, which then got rejected when
looking for 'device' instance for an event handler. The end result was
that device-added handler got VirtualDevice() instance, instead of
DeviceInfo() and that made qui-domains widget unhappy:

    Failed to handle event: sys-usb, device-added:usb, {'device': sys-usb+4-1:0bda:8179:00E04C0001:uffffff}
    Traceback (most recent call last):
      File "/usr/lib/python3.13/site-packages/qubesadmin/events/__init__.py", line 278, in handle
        kwargs['device'] = plugged
        ^^^^^^^^^^^^^^^^^^^^^
      File "/usr/lib/python3.13/site-packages/qui/devices/device_widget.py", line 262, in device_added
        dev = backend.Device(device, self)
      File "/usr/lib/python3.13/site-packages/qui/devices/backend.py", line 156, in __init__
        for interface in dev.interfaces:
                         ^^^^^^^^^^^^^^
    AttributeError: 'VirtualDevice' object has no attribute 'interfaces'

There were several issues:
- using 'elif' made the code never called, as 'device-removed' event is
  handled earlier too
- "port" is not the same as "port id" - the former is
  backend-name:port-id
- the actual line missed "del"

Fixes: c95a89c "devices: invalidate device cache on device-removed event"
Fixes QubesOS/qubes-issues#10674

qubesadmin/events/__init__.py
@@ -257,10 +257,11 @@ class EventsDispatcher:
         ):
             devclass = event.split(":")[1]
             subject.devices[devclass]._attachment_cache = None
-        elif event.split(":")[0] in ("device-removed",):
+        if event.split(":")[0] in ("device-removed",):
             devclass = event.split(":")[1]
+            port_id = kwargs.get("port", ":").split(":")[1]
             try:
-                subject.devices[devclass]._dev_cache[kwargs["port"]]
+                del subject.devices[devclass]._dev_cache[port_id]
             except KeyError:
                 pass
 