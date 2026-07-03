46b1851e913c11a88517566d4a54359570183b3b
Rusty Bird <rustybird@net-c.com>
2026-07-03 13:05:33 +0000

    tools/qvm-volume: use 'resize' in code instead of old 'extend'
    'extend' is already just a legacy alias for 'resize'. It looked odd to
    continue using the former name in the implementation, since it has been
    able to shrink volumes for a long time.

--- qubesadmin/tools/qvm_volume.py
+++ qubesadmin/tools/qvm_volume.py
@@ -242,8 +242,8 @@ def revert_volume(args):
     volume.revert(revision)
 
 
-def extend_volumes(args):
-    """ Called by the parser to execute the :program:`qvm-volume extend`
+def resize_volumes(args):
+    """ Called by the parser to execute the :program:`qvm-volume resize`
         subcommand
     """
     volume = args.volume
@@ -291,18 +291,18 @@ def init_revert_parser(sub_parsers):
     revert_parser.set_defaults(func=revert_volume)
 
 
-def init_extend_parser(sub_parsers):
-    """ Add 'extend' action related options """
-    extend_parser = sub_parsers.add_parser(
+def init_resize_parser(sub_parsers):
+    """ Add 'resize' action related options """
+    resize_parser = sub_parsers.add_parser(
         "resize", aliases=('extend', ), help="resize volume for domain")
-    extend_parser.add_argument(metavar='VM:VOLUME', dest='volume',
+    resize_parser.add_argument(metavar='VM:VOLUME', dest='volume',
                                action=qubesadmin.tools.VMVolumeAction)
-    extend_parser.add_argument('size', help='New size in bytes')
-    extend_parser.add_argument(
+    resize_parser.add_argument('size', help='New size in bytes')
+    resize_parser.add_argument(
         '--force', '-f', action='store_true',
         help='Force operation, even if new size is smaller than the current '
              'one')
-    extend_parser.set_defaults(func=extend_volumes)
+    resize_parser.set_defaults(func=resize_volumes)
 
 
 def init_info_parser(sub_parsers):
@@ -371,7 +371,7 @@ def get_parser():
         dest='command')
     init_info_parser(sub_parsers)
     init_config_parser(sub_parsers)
-    init_extend_parser(sub_parsers)
+    init_resize_parser(sub_parsers)
     init_list_parser(sub_parsers)
     init_revert_parser(sub_parsers)
     init_import_parser(sub_parsers)
